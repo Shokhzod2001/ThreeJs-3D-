1e1a488e23bd09f1a9f426af7474064f8b26d535
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-11-09 00:33:08 +0900

    fix: modify deploying process

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@ git reset --hard
 git checkout master
 git pull origin master
 
+npm install yarn -g
+yarn global add serve
 yarn
 yarn run build
-pm2 start yarn --name "threejs-app" -- start
+pm2 start start.js --name "Three.js" 
